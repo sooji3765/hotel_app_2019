afba6341c6c18dbe564dee317ad068bb01737957
sooji3765 <0711hsj@gmail.com>
2019-11-13 02:20:40 +0900

    README COMMIT

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
-# Hotel WEB 
+# Hotel Web
 
- REACT WEB
+    REACT WEB, HOTEL INFO , FILTER 
+
+## SITE
+
+    https://sooji3765.github.io/hotel_app_2019
+    - HOME 버튼을 클릭해 주세요. 
 
 
+## START
+
+    <code>
+        yarn build
+    </code>
+
+    <code>
+        yarn run deploy
+    </code>
--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-
 import Home from './pages/Home';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
 import {Route, Switch} from 'react-router-dom';
 import Navbar from './components/Navbar';
+
 
 function App() {
   return (
